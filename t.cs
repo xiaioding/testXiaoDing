a4c59bdb44185dff21746baf1d7085d5afd50191
xiaoding <2315643446@qq.com>
2023-08-11 14:44:01 +0800

    #[feature] --修改了类的内容

--- t.cs
+++ t.cs
@@ -1,3 +1,11 @@
-private class lei{
-  
+private class lei
+{
+    private string a = "123456";
+    private string b = "78910";
+
+    private string calcu(string a, string b)
+    {
+        return a + b;
+    }
+
 }
